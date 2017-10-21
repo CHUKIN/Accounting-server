7bd6d5d28cd856ef8321dced992e707ba8d3f76e
Дмитрий Атаманчук <dmitriyatamanchuk@gmail.com>
2017-10-21 23:24:12 +0300

    Include MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace EmptyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
